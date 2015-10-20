4a8a7bfad5325d19ffefba7c4fc85822ab897654
Andrew de Rozario <andrew_de_rozario@hotmail.com>
2015-10-20 20:29:38 +0100

    switching to using resource and arrow syntax to correct this

--- LearningTypeScriptClient/src/app/product/controllers/product.controller.ts
+++ LearningTypeScriptClient/src/app/product/controllers/product.controller.ts
@@ -1,25 +1,26 @@
 module inventoryApp.store {
     
     export class ProductController {
-        public name: string = "Te";
-        public code: string = "Te";
+        public name: string;
+        public code: string;
+        public product: inventoryApp.models.IProduct;
         
         constructor(private $stateParams: ng.ui.IStateParamsService,private $resource: any,private $http: angular.IHttpService){
             var productResource = $resource('https://andrewlearningtypescript.azurewebsites.net/products/:productId', {productId:'@id'});
             //TODO refactor into service
-            // var product = productResource.get({productId:$stateParams['id']}, function(data) {
-            //     this.name = ""//data.name;
-            //     this.code = ""//data.code;
-            // }); 
-            $http(<angular.IRequestConfig>{
-                url: 'https://andrewlearningtypescript.azurewebsites.net/products/'+$stateParams['id'],
-                method: 'GET',
-                dataType: 'json'
-            })
-                .then((result: angular.IHttpPromiseCallbackArg<inventoryApp.models.IProduct>) => {
-                        this.name = result.data.name;
-                        this.code = result.data.code;
-                });
+            productResource.get({productId:$stateParams['id']}, (data)=> {
+                this.code = data.code;
+                this.name = data.name;
+            }); 
+            // $http(<angular.IRequestConfig>{
+            //     url: 'https://andrewlearningtypescript.azurewebsites.net/products/'+$stateParams['id'],
+            //     method: 'GET',
+            //     dataType: 'json'
+            // })
+            //     .then((result: angular.IHttpPromiseCallbackArg<inventoryApp.models.IProduct>) => {
+            //             this.name = result.data.name;
+            //             this.code = result.data.code;
+            //     });
         }
         
         update():void {
