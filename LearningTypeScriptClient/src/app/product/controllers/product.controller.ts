6e1dddb0ef31a064ca591e02577c94fb656d2451
Andrew de Rozario <andrewderozario@andrews-mbp.home>
2015-10-24 16:48:32 +0100

    added create and delete to product module

--- LearningTypeScriptClient/src/app/product/controllers/product.controller.ts
+++ LearningTypeScriptClient/src/app/product/controllers/product.controller.ts
@@ -1,29 +1,40 @@
 module inventoryApp.store {
-    
+
     export interface IProductController {
-        create(): void;
-        update(): void;
+        createOrUpdate(): void;
+        delete(): void;
     }
-    
+
     export class ProductController implements IProductController {
-        
-        constructor(public product: inventoryApp.models.IApiProduct, public productResource: IProductResource, private toastr: Toastr){
+
+        constructor(public product: inventoryApp.models.IApiProduct, public productResource: IProductResource, private toastr: Toastr) {
 
         }
-        
-        create(): void {
-            
-        }
-        
-        update():void {
-            this.productResource.update({id: this.product.id},this.product)
-            .$promise.then(()=>{
-                this.toastr.success("Sucessfully update product details.");
+
+        createOrUpdate(): void {
+            if (typeof this.product.id === 'undefined') {
+                this.product.$save()
+                    .then(() => {
+                        this.toastr.success("Sucessfully created product.");
+                    });
+            } else {
+                this.productResource.update({ id: this.product.id }, this.product)
+                    .$promise.then(() => {
+                        this.toastr.success("Sucessfully update product details.");
+                    });
+            }
+
+
+        };
+
+        delete(): void {
+            this.product.$delete()
+            .then(()=>{
+                this.toastr.success("Sucessfully deleted product.");
             });
-            
         };
     }
-    
-    angular.module('inventoryApp').controller('ProductController',ProductController);
-    
+
+    angular.module('inventoryApp').controller('ProductController', ProductController);
+
 }
--- LearningTypeScriptClient/src/app/product/routes/store.routes.ts
+++ LearningTypeScriptClient/src/app/product/routes/store.routes.ts
@@ -47,9 +47,20 @@ module inventoryApp.store {
           return productResource.query().$promise;
     }
     
-    productResolver.$inject = ['productResource','$stateParams'];
-    function productResolver(productResource: IProductResource, $stateParams: ng.ui.IStateParamsService){
-          return productResource.get({productId:$stateParams['id']}).$promise
+    productResolver.$inject = ['productResource','$stateParams','$q'];
+    function productResolver(productResource: IProductResource, $stateParams: ng.ui.IStateParamsService,$q :angular.IQService){
+      
+      var id = $stateParams['id'];
+      //new 
+      if(id==='0'){
+        var promise = $q.defer();
+        promise.resolve(new productResource());
+        return promise.promise;
+      }else{
+        return productResource.get({productId:id}).$promise
+      }
+      
+          
     }
     
     angular.module('inventoryApp').config(RouterConfig);
