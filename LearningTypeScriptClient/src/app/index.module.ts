0d97102f0f61bb8496d3cf19a5157e835b7140e5
Andrew de Rozario <andrewderozario@andrews-mbp.home>
2015-10-25 10:27:59 +0000

    removing unused stuff left from yeoman

--- LearningTypeScriptClient/src/app/index.module.ts
+++ LearningTypeScriptClient/src/app/index.module.ts
@@ -7,9 +7,6 @@
 /// <reference path="index.run.ts" />
 /// <reference path="main/main.controller.ts" />
 /// <reference path="../app/components/navbar/navbar.directive.ts" />
-/// <reference path="../app/components/malarkey/malarkey.directive.ts" />
-/// <reference path="../app/components/webDevTec/webDevTec.service.ts" />
-/// <reference path="../app/components/githubContributor/githubContributor.service.ts" />
 
 declare var malarkey: any;
 declare var toastr: Toastr;
@@ -27,9 +24,6 @@ module inventoryApp {
     .config(RouterConfig)
 
     .run(RunBlock)
-    .service('githubContributor', GithubContributor)
-    .service('webDevTec', WebDevTecService)
     .controller('MainController', MainController)
-    .directive('acmeNavbar', acmeNavbar)
-    .directive('acmeMalarkey', acmeMalarkey);
+    .directive('acmeNavbar', acmeNavbar);
 }
